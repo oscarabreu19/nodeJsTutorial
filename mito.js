04317b5f515795aa50da3a20eb65eb66bd309b51
Oscar Galindez <ogalindez@teceng-gaming.com.ar>
2020-11-13 08:03:20 -0300

    [requireExports][add] It is created different examples about the definition of arrow functions

--- mito.js
+++ mito.js
@@ -1,6 +1,11 @@
 module.exports = {
-    greet: function () {  // Expose to function
-        console.log('Suscribete al canal');
+    subscribers: 22000, // Expose to variable
+    additionUnique: a => a + 10,
+    additionTwo: (a, b) => a + b,
+    addition: (a, b) => {
+        return a + b;
     },
-    subscribers: 22000 // Expose to variable
+    greet: () => {  // Expose to function
+        console.log('Suscribete al canal');
+    }
 };
