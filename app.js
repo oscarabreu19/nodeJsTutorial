53125584dc8f6c72dadd549f35d91c2598fe5e6d
Oscar Galindez <ogalindez@teceng-gaming.com.ar>
2020-11-13 08:09:39 -0300

    [requireExports][add] Differents ways to invoke to arrow functions

--- app.js
+++ app.js
@@ -3,8 +3,17 @@ const fs = require('fs');
 const mi = require('./mito.js');
 
 // Consume what was expose from the module 'mito.js'
-mi.greet();
-console.log(mi.subscribers);
+myModule.greet();
+console.log(myModule.subscribers);
 
 // Here we can see what is 'module'
-console.log(module);
+// console.log(module);
+
+const result = myModule.addition(3, 4);
+console.log(result);
+
+const resultTwo = myModule.additionTwo(8, 4);
+console.log(resultTwo);
+
+const resultUnique = myModule.additionUnique(4);
+console.log(resultUnique);
