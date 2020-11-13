b45ade1d5d05eb10dcd81797445a523585cbecc9
Oscar Galindez <ogalindez@teceng-gaming.com.ar>
2020-11-13 08:14:28 -0300

    [requireExports][add] How is the behavior of the setTimeout about: to second callstack, time's param and returns for applay clearInterval

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const os = require('os');
 const fs = require('fs');
-const mi = require('./mito.js');
+const myModule = require('./aModule.js');
+
+const interval = setInterval(() => {
+    console.log('This is printed last');
+    clearTimeout(interval);
+}, 3000);
+
+const intervalTwo = setInterval(() => {
+    console.log('This is printed before');
+    clearTimeout(intervalTwo);
+});
+
 
 // Consume what was expose from the module 'mito.js'
 myModule.greet();
