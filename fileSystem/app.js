4a93e76b1f1cceea3fc21de65514bce3198d60d8
Oscar Galindez <ogalindez@teceng-gaming.com.ar>
2020-11-14 16:32:07 -0300

    [fileSystem][add] renamed of a file asynchonously

--- fileSystem/app.js
+++ fileSystem/app.js
@@ -1,6 +1,14 @@
 const fs = require('fs');
 
 console.log('Iniciado');
+
 const fileData = fs.readFileSync('./data.txt', 'utf-8');
+
 console.log(fileData);
+
+fs.rename('./data.txt', 'dataRenamed.txt', (error, data) => {
+    if (error) throw error;
+    console.log('Renamed!');
+});
+
 console.log('Finalizado');
