6376bbdddf8b281b8cd3ff506dc53256db4e32c8
Oscar Galindez <ogalindez@teceng-gaming.com.ar>
2020-11-14 16:47:51 -0300

    [fileSystem][refactor] getting better to algorithm

--- fileSystem/app.js
+++ fileSystem/app.js
@@ -6,8 +6,8 @@ const fileData = fs.readFileSync('./data.txt', 'utf-8');
 
 console.log(fileData);
 
-fs.appendFile('./data.txt', '. This is a new text!', (e) => {
-    if(e) throw e;
+fs.appendFile('./data.txt', '\nThis is a new text!', (e) => {
+    if(e) console.log(e);
     console.log('Appended new text!');
 });
 
