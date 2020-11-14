019821740caa3394ddb2fb1d682ba0bbdbb57067
Oscar Galindez <ogalindez@teceng-gaming.com.ar>
2020-11-14 15:50:00 -0300

    [fileSystem][add] reading a file synchronously

--- fileSystem/app.js
+++ fileSystem/app.js
@@ -1,11 +1,6 @@
 const fs = require('fs');
 
 console.log('Iniciado');
-fs.readFile('./data.txt', 'utf-8', (error, data) => {
-    if (error) {
-        console.log(`Error ${error}`);
-        return;
-    }
-    console.log(data);
-});
+const fileData = fs.readFileSync('./data.txt', 'utf-8');
+console.log(fileData);
 console.log('Finalizado');
